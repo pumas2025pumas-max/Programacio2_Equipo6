512262c96abbd2dcc1aaa999bea3770f440ed144
pumas2025pumas-max <pumas2025pumas@gmail.com>
2026-04-24 13:53:24 -0300

    a

--- Programacion2-master-main/Parcial_1_Grupo_06/src/stackModule/SimpleStack.java
+++ Programacion2-master-main/Parcial_1_Grupo_06/src/stackModule/SimpleStack.java
@@ -2,9 +2,14 @@ package stackModule;
 
 public interface SimpleStack<E> {
 	public void push(E element);
+
 	public E pop();
+
 	public E peek();
+
 	public void clear();
+
 	public int size();
+
 	public boolean isEmpty();
 }
